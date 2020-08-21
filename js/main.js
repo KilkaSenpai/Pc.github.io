df184af75e14d7281eb56f7fb97363ae5d5107a5
KilkaSenpai <grekagrove@gmail.com>
2020-08-21 07:38:05 +0300

    added revisions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(function () {
-  $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true
-  });
-
   $('.product-slider__inner').slick({
     dots: true,
     arrows: false,
